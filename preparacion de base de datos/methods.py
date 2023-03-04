4dbb4096fdd1f3a35d2d8786e6f74906c8ec666d
ElPP <pableitorm@gmail.com>
2023-03-04 17:51:09 -0500

    lista con acci 1
    Please enter the commit message for your changes. Lines starting

--- preparacion de base de datos/methods.py
+++ preparacion de base de datos/methods.py
@@ -13,11 +13,21 @@ def creacion():
 def conversion_asky(palabra):
     cadascii=""
     infrm=[]
-    for s in palabra:
-        codigo_ascii = ord(s)
-        codigo_ascii=str(codigo_ascii)
-        cadascii+=codigo_ascii
-        inf=codigo_ascii+"-"+s
-        infrm.append(inf)
+    print(palabra,"<--- palabra")
+    try:
+        for s in palabra:
+            codigo_ascii = ord(s)
+            if s==" " or s=="-" or s=="_":
+                cadascii+=" "
+            else:
+                codigo_ascii=str(codigo_ascii)
+                codigo_ascii+=" "
+                cadascii+=codigo_ascii
+                inf=codigo_ascii+"-"+s
+                infrm.append(inf)
+    except:
+        print("no se paso ningun parametro")
+        return("N/E")
     print("El Codigo Ascii de la palabra es: ",infrm)
+    print(cadascii)
     return cadascii
